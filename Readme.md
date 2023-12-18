e3350ebea97bab51cd3581da8621605ab395437c
elmalditoemi <emijoaco_pereyra@hotmail.com>
2023-12-18 08:55:18 -0300

    Tomando un modelo POO

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 arsac stands for Awful rsa cracker
 
 just createad as an excuse to try making GUI's with GTK and python
+
+
+## Documentacion
+- [gtk3 y python](https://python-gtk-3-tutorial.readthedocs.io/en/latest/basics.html)
--- src/main.py
+++ src/main.py
@@ -1,23 +1,23 @@
-# Load Gtk
+import sys
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
 
-# When the application is launched…
-def on_activate(app):
-    # … create a new window…
-    win = Gtk.ApplicationWindow(application=app)
-    win.
-    # … with a button in it…
-    btn = Gtk.Button(label='Hello, World!')
-    # … which closes the window when clicked
-    btn.connect('clicked', lambda x: win.close())
-    win.set_child(btn)
-    win.present()
 
-# Create a new application
-app = Gtk.Application(application_id='com.example.GtkApplication')
-app.connect('activate', on_activate)
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Things will go here
 
-# Run the application
-app.run(None)
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp(application_id="com.example.GtkApplication")
+app.run(sys.argv)
